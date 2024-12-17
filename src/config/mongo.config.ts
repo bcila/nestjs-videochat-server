6a6f6b2dd1d233cddc31eb3b897dd18828a55440
bcila <b.cila51@gmail.com>
2024-12-17 23:26:51 +0300

    chore: mongo uri draft moved to env

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -6,5 +6,5 @@ export default registerAs('mongo', () => ({
   password: process.env.MONGO_PASSWORD,
   host: process.env.MONGO_HOST,
   appName: process.env.MONGO_APPNAME,
-  uri: `mongodb+srv://<mongo_username>:<mongo_password>@<mongo_host>/?retryWrites=true&w=majority&appName=<mongo_appName>`,
+  uri: process.env.MONGO_URI,
 }));
